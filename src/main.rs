4679f8bded8facc4f19a6dc0796ba88a9d7e9b43
Andrew Demonov <fcoder@live.com>
2019-03-21 00:11:36 -0400

    tokio thread

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use env_logger;
 use futures::Future;
 use log;
 use std::env;
+use std::time::Duration;
 
 fn handle_message(context: &mut Context, message: &Message) -> HandlerFuture {
     log::info!("got a message: {:?}\n", message);
@@ -19,26 +20,53 @@ fn handle_message(context: &mut Context, message: &Message) -> HandlerFuture {
     HandlerResult::Continue.into()
 }
 
+//fn create_thread(api: Api) {
+//    std::thread::spawn(move || {
+//        let method = SendMessage::new(-1001369415711, "Hello");
+//        let response = api.execute(&method).then(|x| {
+//            log::info!("sendMessage result: {:?}\n", x);
+//            futures::future::ok::<u32, u32>(7)
+//        });
+//    });
+//}
+
+//
+//fn create_future() -> impl Future<Item = (), Error = ()> {
+//    log::info!("11");
+//    futures::future::ok(())
+//}
+
+fn create_thread(api: Api) {
+    std::thread::spawn(move || {
+        log::info!("Tokio thread starting...");
+        tokio::run(
+            App::new()
+                .add_handler(Handler::message(handle_message))
+                .run(api.clone(), UpdateMethod::poll(UpdatesStream::new(api)))
+        );
+    });
+}
+
 fn main() {
     dotenv().expect("Failed to read .env file");;
     env_logger::init();
     log::info!("started");
 
-    //let token = env::var("CARAPAX_TOKEN").expect("CARAPAX_TOKEN is not set");
-    
+    let token = env::var("CARAPAX_TOKEN").expect("CARAPAX_TOKEN is not set");
     let proxy = env::var("CARAPAX_PROXY").ok();
 
     let api = Api::new(token, proxy).unwrap();
-    tokio::run(
-        App::new()
-            .add_handler(Handler::message(handle_message))
-            .run(api.clone(), UpdateMethod::poll(UpdatesStream::new(api))),
-    );
+    create_thread(api.clone());
+
+    std::thread::sleep(Duration::from_secs(5));
 
     let method = SendMessage::new(-1001369415711, "Hello");
-    api.clone().execute(&method).then(|x| {
-        log::info!("sendMessage result: {:?}\n", x);
-        Ok()
-    });
+    let f = api.execute(&method);
+
+    let mut reactor = Core::new().unwrap();
+    reactor.run(f).unwrap();
+
+    //log::info!("sendMessage result: {:?}\n", r);
+    std::thread::sleep(Duration::from_secs(5));
 }
 
